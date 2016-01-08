c1e3c07c84be9cfe668e7669345e94e9fa24cf23
monik <monik@bitbucket.org>
2016-01-08 16:10:48 +0530

    home page changes

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -4,49 +4,49 @@ $(document).ready(function() {
 
     $('#toggle').click(function(){
     	if($('#nameform').css('display')=='none'){
-            $('#toggle').text('');
+            //$('#toggle').text('');
 $('#categoryform').fadeToggle(100,function() {
     $('#nameform').fadeToggle(100, function(){
-         $('#toggle').text('Search By Category?');
+         $('#toggle').text('Search by category');
     });
 })};
    if($('#categoryform').css('display')=='none'){
-       $('#toggle').text('');
+       //$('#toggle').text('');
 $('#nameform').fadeToggle(100, function(){
     $('#categoryform').fadeToggle(100,function(){
-        $('#toggle').text('Search By Name?');
+        $('#toggle').text('Search by name');
     });
 })};
     });
 
-  var slider = document.getElementById('slider');
-  noUiSlider.create(slider, {
-	start: [10000, 100000],
-	connect: true,
-	range: {
-		'min': 2000,
-		'max': 200000
-	},
-	step: 1000
-});
-            $('#id_budget_min').val(slider.noUiSlider.get()[0]);
-            $('#id_budget_max').val(slider.noUiSlider.get()[1]);
-            $('#budgetmin').text("Rs. " + Math.ceil($('#id_budget_min').val()));
-            $('#budgetmax').text("Rs. " + Math.ceil($('#id_budget_max').val()));
-slider.noUiSlider.on('slide', function(){
-$('#id_budget_min').val(slider.noUiSlider.get()[0]);
-            $('#id_budget_max').val(slider.noUiSlider.get()[1]);
-            $('#budgetmin').text("Rs. " + Math.ceil($('#id_budget_min').val()));
-            $('#budgetmax').text("Rs. " + Math.ceil($('#id_budget_max').val()));
-
-        });
-slider.noUiSlider.on('change', function(){
-$('#id_budget_min').val(slider.noUiSlider.get()[0]);
-            $('#id_budget_max').val(slider.noUiSlider.get()[1]);
-            $('#dropdownMenu1').text("Rs. " + Math.ceil($('#id_budget_min').val()) +" -  " + Math.ceil($('#id_budget_max').val()));
-
-        });
-$('#dropdownMenu1').text("Budget");
+//  var slider = document.getElementById('slider');
+//  noUiSlider.create(slider, {
+//	start: [10000, 100000],
+//	connect: true,
+//	range: {
+//		'min': 2000,
+//		'max': 200000
+//	},
+//	step: 1000
+//});
+//            $('#id_budget_min').val(slider.noUiSlider.get()[0]);
+//            $('#id_budget_max').val(slider.noUiSlider.get()[1]);
+//            $('#budgetmin').text("Rs. " + Math.ceil($('#id_budget_min').val()));
+//            $('#budgetmax').text("Rs. " + Math.ceil($('#id_budget_max').val()));
+//slider.noUiSlider.on('slide', function(){
+//$('#id_budget_min').val(slider.noUiSlider.get()[0]);
+//            $('#id_budget_max').val(slider.noUiSlider.get()[1]);
+//            $('#budgetmin').text("Rs. " + Math.ceil($('#id_budget_min').val()));
+//            $('#budgetmax').text("Rs. " + Math.ceil($('#id_budget_max').val()));
+//
+//        });
+//slider.noUiSlider.on('change', function(){
+//$('#id_budget_min').val(slider.noUiSlider.get()[0]);
+//            $('#id_budget_max').val(slider.noUiSlider.get()[1]);
+//            $('#dropdownMenu1').text("Rs. " + Math.ceil($('#id_budget_min').val()) +" -  " + Math.ceil($('#id_budget_max').val()));
+//
+//        });
+//$('#dropdownMenu1').text("Budget");
 
 
     $("#nameform input").autocomplete({
