aa684d95b17fad205ce5a336f6d3c42847e142c7
Suhail Barot <suhailbarot@Suhails-MacBook-Pro.local>
2016-01-09 18:52:18 +0530

    user login/register modal updated, homepage navbar collapse added

--- app/forms.py
+++ app/forms.py
@@ -87,7 +87,7 @@ class RegisterForm(forms.Form):
     phone = forms.CharField(validators=[number_validator], widget=forms.TextInput(attrs=dict(maxlength=12)),
                             label=u'Phone ')
     password1 = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict, render_value=False), label=u'Password')
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict, render_value=False), label=u'Re-Type Password')
+    #password2 = forms.CharField(widget=forms.PasswordInput(attrs=attrs_dict, render_value=False), label=u'Re-Type Password')
 
     def clean_email(self):
         """
@@ -100,15 +100,15 @@ class RegisterForm(forms.Form):
             return self.cleaned_data['email']
         raise forms.ValidationError(u'Seems like the email is already in use. Please log in instead')
 
-    def clean(self):
-        """
-        check passwords entered are the same
-        :return:
-        """
-        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
-            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
-                raise forms.ValidationError(u'You must type the same password each time')
-        return self.cleaned_data
+    # def clean(self):
+    #     """
+    #     check passwords entered are the same
+    #     :return:
+    #     """
+    #     if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+    #         if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+    #             raise forms.ValidationError(u'You must type the same password each time')
+    #     return self.cleaned_data
 
     def save(self, actype):
         name = self.cleaned_data['name']
